9ca70833b6ad49950694e1470cd79614e43010a1
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-14 12:57:40 +0300

    Fix: element names.

--- src/root.rs
+++ src/root.rs
@@ -36,8 +36,8 @@ impl View for RootView {
         let gbuilder = gtk::Builder::from_resource("/org/altereigo/npaf/Root.glade");
         let (p_menubar, p_toolbar, p_workspace) = (
             gbuilder.object::<gtk::Grid>("p_menu_bar").unwrap(),
-            gbuilder.object::<gtk::Grid>("p_menu_bar").unwrap(),
-            gbuilder.object::<gtk::Grid>("p_menu_bar").unwrap(),
+            gbuilder.object::<gtk::Grid>("p_tool_bar").unwrap(),
+            gbuilder.object::<gtk::Grid>("p_workspace").unwrap(),
         );
         let menubar = MenuBarView::new().assemble();
         p_menubar.attach(&menubar, 0, 0, 1, 1);

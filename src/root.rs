fee8980bdcd046950032d2aaf3e5ec23c2be1321
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-14 12:41:36 +0300

    Add: view for MenuBar.

--- src/root.rs
+++ src/root.rs
@@ -3,6 +3,19 @@ use gtk::prelude::*;
 use crate::prelude::View;
 use std::rc::Rc;
 
+pub struct MenuBarView {
+
+}
+
+impl View for MenuBarView {
+    fn assemble(&self) -> gtk::Widget {
+        let gbuilder = gtk::Builder::from_resource("/org/altereigo/npaf/MenuBar.glade");
+        let grid: gtk::Grid = gbuilder.object("root").unwrap();
+        grid.show();
+        grid.dynamic_cast::<gtk::Widget>().unwrap()
+    }
+}
+
 pub struct RootView {
 }
 

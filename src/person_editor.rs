527f283d2c5389a2652e299459cd6bf819614503
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-17 10:52:11 +0300

    Add: trait for generating the integrated window.

--- src/person_editor.rs
+++ src/person_editor.rs
@@ -5,18 +5,29 @@ use crate::{
 };
 
 pub struct PersonEditorView {
+    gbuilder: gtk::Builder
 }
 
 impl PersonEditorView {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            gbuilder: gtk::Builder::from_resource("/org/altereigo/npaf/PersonEditor.glade")
+        }
     }
 }
 
 impl View for PersonEditorView {
     fn assemble(&self) -> gtk::Widget {
-        let gbuilder = gtk::Builder::from_resource("/org/altereigo/npaf/PersonEditor.glade");
-        let root: gtk::Grid = gbuilder.object("root").unwrap();
+        let root: gtk::Grid = self.gbuilder.object("root").unwrap();
         root.dynamic_cast::<gtk::Widget>().unwrap()
+    }
+}
+
+impl Windowed for PersonEditorView {
+    fn assemble_window(&self) -> gtk::Window {
+        let wdg = self.assemble();
+        let wdw: gtk::Window = self.gbuilder.object("top").unwrap();
+        wdw.set_child(Some(&wdg));
+        wdw
     }
 }

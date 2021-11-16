e016e8cf9356916c4f1e32c9a83d0dd1a718142f
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-11-16 17:52:50 +0300

    Add: extracting input file in memory.

--- src/gedcom/mod.rs
+++ src/gedcom/mod.rs
@@ -1,4 +1,6 @@
 use std::rc::Rc;
+use std::io::{BufReader, BufRead};
+use std::fs::File;
 use std::cell::RefCell;
 use std::collections::HashMap;
 
@@ -10,12 +12,25 @@ pub struct Record {
     children: Vec<Rc<RefCell<Record>>>
 }
 
+type IOError = std::io::Error;
+
+enum ParseError {
+    IO(IOError)
+}
+
+impl From<IOError> for ParseError {
+    fn from(o: IOError) -> ParseError {
+        ParseError::IO(o)
+    }
+}
+
 type RecordRegistry = HashMap<u64, Rc<RefCell<Record>>>;
+type ParseResult = Result<RecordRegistry, ParseError>;
 
 pub trait Parser {
     type FileType;
 
-    fn parse(&mut self, file: &Self::FileType) -> RecordRegistry;
+    fn parse(&mut self, file: &Self::FileType) -> ParseResult;
 }
 
 #[derive(Default)]
@@ -30,8 +45,10 @@ impl Buildable for GedParser {
 impl Parser for GedParser {
     type FileType = std::fs::File;
 
-    fn parse(&mut self, file: &Self::FileType) -> RecordRegistry {
-        RecordRegistry::new()
+    fn parse(&mut self, file: &Self::FileType) -> ParseResult {
+        let mut reader = BufReader::new(file);
+        let contents = reader.lines().map(|l| l.unwrap());
+        Ok(RecordRegistry::new())
     }
 }
 

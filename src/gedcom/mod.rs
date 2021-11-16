dbb1710e68c6fa984f53aeea20bf8b6d95698ea8
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-11-16 15:49:36 +0300

    Add: id field for a record and parse method for the parser trait.

--- src/gedcom/mod.rs
+++ src/gedcom/mod.rs
@@ -1,13 +1,19 @@
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::collections::HashMap;
 
 pub struct Record {
+    id: u64,
     name: String,
     father: Option<Rc<RefCell<Record>>>,
     mother: Option<Rc<RefCell<Record>>>,
     children: Vec<Rc<RefCell<Record>>>
 }
 
+type RecordRegistry = HashMap<u64, Rc<RefCell<Record>>>;
+
 pub trait Parser {
-    
+    type FileType;
+
+    fn parse(&mut self, file: &Self::FileType) -> RecordRegistry;
 }

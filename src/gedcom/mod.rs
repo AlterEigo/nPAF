92822e1741ca4288f377e167f029335745c7f498
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-11-17 14:13:50 +0300

    Add: line parser method.

--- src/gedcom/mod.rs
+++ src/gedcom/mod.rs
@@ -58,6 +58,27 @@ impl GedParser {
             .fold(0, |acc, _l| acc + 1)
     }
 
+    fn parse_line(line: &str) -> Option<GedLine> {
+        let r_data = Self::regex_line();
+        let r_ref = Self::regex_ref();
+
+        if let Some(caps) = r_data.captures(&line) {
+            Some(GedLine::Data(
+                caps.name("Level").unwrap().as_str().parse().unwrap(),
+                caps.name("Tag").unwrap().as_str().to_owned(),
+                if let Some(content) = caps.name("Content") { Some(content.as_str().to_owned()) } else { None }
+            ))
+        } else if let Some(caps) = r_ref.captures(&line) {
+            Some(GedLine::Ref(
+                caps.name("Level").unwrap().as_str().parse().unwrap(),
+                caps.name("Tag").unwrap().as_str().to_owned(),
+                if let Some(content) = caps.name("Content") { Some(content.as_str().to_owned()) } else { None }
+            ))
+        } else {
+            None
+        }
+    }
+
     fn regex_line() -> Regex {
         Regex::new(r"(?x) # Insignificant whitespace mode
                 ^
@@ -92,23 +113,7 @@ impl Parser for GedParser {
         let re_ref = GedParser::regex_ref();
         let contents = reader.lines()
             .filter_map(|l| l.ok())
-            .filter_map(|l| {
-                if let Some(caps) = re.captures(&l) {
-                    Some(GedLine::Data(
-                        caps.name("Level").unwrap().as_str().parse().unwrap(),
-                        caps.name("Tag").unwrap().as_str().to_owned(),
-                        if let Some(content) = caps.name("Content") { Some(content.as_str().to_owned()) } else { None }
-                    ))
-                } else if let Some(caps) = re_ref.captures(&l) {
-                    Some(GedLine::Ref(
-                        caps.name("Level").unwrap().as_str().parse().unwrap(),
-                        caps.name("Tag").unwrap().as_str().to_owned(),
-                        if let Some(content) = caps.name("Content") { Some(content.as_str().to_owned()) } else { None }
-                    ))
-                } else {
-                    None
-                }
-            });
+            .filter_map(|l| Self::parse_line(&l));
         Ok(RecordRegistry::new())
     }
 }

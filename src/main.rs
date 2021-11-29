6cd0ea852aad356bf862eba85b8f6dfa8277780e
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-11-29 15:34:06 +0300

    Add: 'main' function version for tests.

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,23 @@ mod gedcom;
 use crate::prelude::*;
 use gtk::prelude::*;
 use std::rc::Rc;
+use gedcom::*;
 
 fn main() {
+    let mut parser: GedParser = Default::default();
+    let data = std::fs::File::open("data_sample.ged").unwrap();
+    let unparsed = parser.count_unparsed(&data);
+    let data = std::fs::File::open("data_sample.ged").unwrap();
+    if unparsed > 0 {
+        println!("Did not parse exactly {} lines.", unparsed);
+    }
+    let res = parser.parse(&data);
+    if let Ok(parsed) = res {
+        println!("{:#?}", parsed);
+    }
+}
+
+fn main2() {
     gtk::init().expect("Could not initialize GTK");
 
     let tm = app::Application::builder().build().unwrap();
